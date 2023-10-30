27c50e46ed7394da0b6fc2ce8cd72ab81227c575
gabriel427131 <gabriel.d.oliveira1@hotmail.com>
2023-10-30 13:53:42 -0600

    Update App.js and add Root.js
    - Add paths and RouterProvider to App.js
    
    - Add Root.js to implement MainNavigation and Outlet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,22 @@
-function App() {
-  return <div></div>;
-}
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import HomePage from "./pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "/events", element: <EventsPage /> },
+      { path: "./events/:eventId", element: <EventDetailPage /> },
+      { path: "/events/new", element: <NewEventPage /> },
+      { path: "/events/:eventId/edit", element: <EditEventPage /> },
+    ],
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
